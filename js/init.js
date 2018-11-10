b405a0a349bf084d79d09092b77ab1d6f78e449b
Alhamsya <alhamsya@gmail.com>
2018-11-10 23:58:43 +0700

    default materialize css

---
+++ js/init.js
@@ -0,0 +1,12 @@
+(function ($) {
+  $(function () {
+
+    $('.button-collapse').sideNav();
+
+  }); // end of document ready
+})(jQuery); // end of jQuery name space
+
+
+function login() {
+  var login_status = true;
+}
